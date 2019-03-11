2233f72b61099f231ab864dfbfb3f1f05d2aebb9
Zishan Ali <zishanali06@gmail.com>
2019-03-10 22:31:42 -0500

    stuck!!

--- dice.js
+++ dice.js
@@ -1,5 +1,10 @@
+// $(document).ready(function () {
+//     let rollbutton = $('<button class="btn btn-primary mt-3 ml-3" id="rolldice">Roll Dice</button>')
+//     $('.buttonrow').append(rollbutton);
+// })
+
 class Dice {
-    constructor(value){
+    constructor(value) {
         this.roll();
         this.newdiv = $('<div class="dice block"></div>');
         this.newdiv.append(this.value);
@@ -7,12 +12,23 @@ class Dice {
     }
 
     roll() {
-        this.value = randomNum()
+        this.value = randomNum();
     }
 };
 
-let randomNum = () => Math.floor(Math.random() * 6) + 1; 
+let randomNum = () => Math.floor(Math.random() * 6) + 1;
 
-$('#generate').click(function(){
+$('#generate').click( () => {
     let newdie = new Dice();
 })
+
+$('#rolldice').click( () => {
+    // console.log(alldice);
+    $('.dice').each(function(e){
+        e.roll();
+        e.newdiv.append(this.value);
+    });
+})
+
+
+
