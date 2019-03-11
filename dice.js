7ef4976e46eebd9a0928b16dad6f3cc8d0c218da
Zishan Ali <zishanali06@gmail.com>
2019-03-11 10:53:05 -0500

    dicey business boom

--- dice.js
+++ dice.js
@@ -2,32 +2,42 @@
 //     let rollbutton = $('<button class="btn btn-primary mt-3 ml-3" id="rolldice">Roll Dice</button>')
 //     $('.buttonrow').append(rollbutton);
 // })
+let diecount = 0;
+let diearray = [];
 
 class Dice {
-    constructor(value) {
-        this.roll();
+    constructor(diecount) {
         this.newdiv = $('<div class="dice block"></div>');
-        this.newdiv.append(this.value);
+        this.roll();
+        this.diecount = diecount;
         $('.diceboard').append(this.newdiv);
     }
 
     roll() {
         this.value = randomNum();
+        this.newdiv.empty();
+        this.newdiv.append(this.value);
     }
 };
 
 let randomNum = () => Math.floor(Math.random() * 6) + 1;
 
-$('#generate').click( () => {
-    let newdie = new Dice();
+$('#generate').click(() => {
+    let newdie = new Dice(diecount);
+    diecount++;
+    diearray.push(newdie);
+    console.log(diearray);
 })
 
-$('#rolldice').click( () => {
-    // console.log(alldice);
-    $('.dice').each(function(e){
-        e.roll();
-        e.newdiv.append(this.value);
-    });
+$('#rolldice').click(() => {
+    let resetdiecount = 0;
+    while (resetdiecount < diearray.length) {
+        let thisdie = diearray.pop();
+        thisdie.roll();
+        diearray.unshift(thisdie);
+        console.log(diearray);
+        resetdiecount++;
+    };
 })
 
 
